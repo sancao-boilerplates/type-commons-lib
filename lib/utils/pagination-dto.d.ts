3246989f8bd5c37f63c2f0441ae9099c1bc1f646
Jefferson Cardoso <jeffersoncardoso.s@gmail.com>
2022-06-19 12:06:40 -0300

    optional pagination fields

--- lib/utils/pagination-dto.d.ts
+++ lib/utils/pagination-dto.d.ts
@@ -1,7 +1,7 @@
 export interface PaginationDto<T, F> {
-    offSet: number;
-    skip: number;
-    total: number;
-    filter: F;
-    data: Array<T>;
+    offSet?: number;
+    skip?: number;
+    total?: number;
+    filter?: F;
+    data?: Array<T>;
 }
