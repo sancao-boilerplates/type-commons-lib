a2c1299116da1eabe8ad70579f10f03f3686e2ef
Jefferson Cardoso <jeffersoncardoso.s@gmail.com>
2022-11-08 14:38:16 -0300

    added automatically token

--- lib/crud-generics/generic-crud-service.d.ts
+++ lib/crud-generics/generic-crud-service.d.ts
@@ -4,6 +4,7 @@ import { PaginationDto } from '../utils/pagination-dto';
 export declare abstract class GenericCrudService<T> extends HttpService {
     resource: string;
     constructor(resource: string, options?: ServiceOptions);
+    private setAuthHeader;
     create(t: T): Promise<T>;
     getAll<F>(pagination: PaginationDto<T, F>): Promise<PaginationDto<T, F>>;
     getById(id: string): Promise<T>;
--- src/crud-generics/generic-crud-service.ts
+++ src/crud-generics/generic-crud-service.ts
@@ -1,4 +1,5 @@
 import { log, Logger } from 'node-smart-log';
+import { AuthSession } from '../auth-decorator';
 import { HttpService } from '../service';
 import { ServiceOptions } from '../service/service-options';
 import { PaginationDto } from '../utils/pagination-dto';
@@ -9,14 +10,24 @@ export abstract class GenericCrudService<T> extends HttpService {
         super(options);
         this.resource = resource;
     }
-
+    private setAuthHeader() {
+        if (AuthSession.getToken()) {
+            const config = this.getRequestConfig();
+            config.headers = config.headers ?? {};
+            config.headers['Authorization'] = AuthSession.getToken();
+            this.setRequestConfig(config);
+            Logger.info('Token added to request header');
+        }
+    }
     @log()
     async create(t: T): Promise<T> {
+        this.setAuthHeader();
         return await this.post(this.resource, t as unknown as object);
     }
 
     @log()
     async getAll<F>(pagination: PaginationDto<T, F>): Promise<PaginationDto<T, F>> {
+        this.setAuthHeader();
         const query = this.getQuery(pagination);
         const url = `${this.resource}${query}`;
         return this.get(url);
@@ -24,18 +35,21 @@ export abstract class GenericCrudService<T> extends HttpService {
 
     @log()
     async getById(id: string): Promise<T> {
+        this.setAuthHeader();
         const url = `${this.resource}/${id}`;
         return this.get(url);
     }
 
     @log()
     async update(id: string, toUpdate: T): Promise<T> {
+        this.setAuthHeader();
         const url = `${this.resource}/${id}`;
         return this.patch(url, toUpdate as unknown as Object);
     }
 
     @log()
     async deleteById(id: string): Promise<void> {
+        this.setAuthHeader();
         const url = `${this.resource}/${id}`;
         return this.delete(url);
     }
